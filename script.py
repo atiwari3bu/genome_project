cd4b881245af547a012b611c9fd01ac713465fb5
atiwari3bu <atiwari3@binghamton.edu>
2018-05-30 23:30:08 -0400

    .

--- script.py
+++ script.py
@@ -1,18 +1,49 @@
 import os
 
+line_number=0
+
+def makeCopy(query,database):
+    print("\nCreating copy of query and database...\n")
+    os.system("cp {} copy_{}".format(query, query)) 
+    os.system("cp {} copy_{}".format(database,database))
+
+
+def runningSelectedVirus(query,database):
+    global line_number
+    makeCopy(query,database)
+    fileref=open("copy_{}".format(query),"r")
+    for row in fileref:
+        if(">" in row):
+            line_number+=1
+            continue
+    
+    print(line_number)
+        
+     
 def main():
+    print("\nRun blast normally?\n ")
+    #a=input()
+    a="no"
+    
     print("\nThe list of files in your directory is: \n")
     os.system("ls")
     print("\nEnter your query file:\n ")
-    query=input()
+    #query=input()
+    query="genomes.fas"
     print("\nEnter your database: \n")
-    database=input()
+    #database=input()
+    database="CP_nuc.fas"
     print("\nCreating nucleotide database for query...\n")
     os.system("makeblastdb -in {} -parse_seqids -dbtype nucl".format(database))
-    print("\nRunning Blast for query with nucleotide database...\n")
-    os.system("blastn -query {} -db {} -out output 2> garbage".format(query,database))
-    print("\nRemoving unnecessary files...\n")
-    os.system("rm *.nin *.nsd *.nsi *.nog *.nsq *.nhr garbage")
-    print("\n Your output file is created with title output...\n")
+    
+    if(a=='YES' or a=='Yes' or a=='yes'):
+        print("\nRunning Blast for query with nucleotide database...\n")
+        os.system("blastn -query {} -db {} -out output 2> garbage".format(query,database))
+        print("\nRemoving unnecessary files...\n")
+        os.system("rm *.nin *.nsd *.nsi *.nog *.nsq *.nhr garbage")
+        print("\n Your output file is created with title output...\n")
 
+    else:
+        print("\nWorking on specific viruses...\n")
+        runningSelectedVirus(query,database)
 main()
